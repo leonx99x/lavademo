3b60807baa00d2a1767c1b398d6cc8e955a7cbfd
leonx99x <koraycill@gmail.com>
2023-07-27 01:12:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header bg-blue-500 text-white p-5">
+        <p>Welcome to React with Tailwind CSS!</p>
       </header>
     </div>
   );
