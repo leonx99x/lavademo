fc82fdb5ae285a6ed70ff51d4c3d77cc42056bd5
leonx99x <koraycill@gmail.com>
2023-07-31 10:40:05 +0300

    ps1 updated

--- docker_run.ps1
+++ docker_run.ps1
@@ -1,8 +1,8 @@
 $image_name = "lavanetworkdemo"
-$tag_name = "latest"
+
 
 # Build Docker image
-docker build -t $image_name:$tag_name .
+docker build -t "$($image_name)" .
 
 # Run Docker container
-docker run -p 8080:80 $image_name:$tag_name
+docker run -p 8080:80 "$($image_name)"
